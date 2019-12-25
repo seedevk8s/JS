fd622b81e0d950a191768b7ebe763f9ae1d62944
chjin <seedevk8s@gmail.com>
2019-12-25 21:43:29 +0900

    getter, setter 함수 만들기

---
+++ index.js
@@ -0,0 +1,32 @@
+const numbers = {
+    _a: 1,
+    _b: 2,
+    sum: 3,
+    calculate() {
+        console.log('calculate');
+        this.sum = this._a + this._b;
+    },
+    get a() {
+        return this._a;
+    },
+    get b() {
+        return this._b;
+    },
+    set a(value) {
+        this._a = value;
+        this.calculate();
+    },
+    set b(value) {
+        this._b = value;
+        this.calculate();
+    }
+};
+
+
+console.log(numbers.sum);
+numbers.a = 5;
+numbers.b = 7;
+numbers.a = 9;
+console.log(numbers.sum);
+
+
