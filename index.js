2b68ee25340a0884fbc9837ebf1dea8af5a94910
chjin <seedevk8s@gmail.com>
2019-12-29 12:39:50 +0900

    for in 함수

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const numbers = [10,20,30,40,50];
-
 const doggy = {
     name: '멍멍이',
     sound: '멍멍',
     age: 2
 };
 
-console.log(Object.entries(doggy));
-console.log(Object.keys(doggy));
-console.log(Object.values(doggy));
+for (let key in doggy) {
+    console.log(`${key}: ${doggy[key]}`);
+}
+
+
 
