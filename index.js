f01772ec665440796c5487690392c24496352458
chjin <seedevk8s@gmail.com>
2019-12-29 12:30:18 +0900

    for of 함수

--- index.js
+++ index.js
@@ -1,32 +1,6 @@
-const numbers = {
-    _a: 1,
-    _b: 2,
-    sum: 3,
-    calculate() {
-        console.log('calculate');
-        this.sum = this._a + this._b;
-    },
-    get a() {
-        return this._a;
-    },
-    get b() {
-        return this._b;
-    },
-    set a(value) {
-        this._a = value;
-        this.calculate();
-    },
-    set b(value) {
-        this._b = value;
-        this.calculate();
-    }
-};
+const numbers = [10,20,30,40,50];
 
-
-console.log(numbers.sum);
-numbers.a = 5;
-numbers.b = 7;
-numbers.a = 9;
-console.log(numbers.sum);
-
+for (let number of numbers) {
+    console.log(number);
+}
 
