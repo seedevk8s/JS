ce115492b1288c1819cd4323a532a5f306da5832
chjin <seedevk8s@gmail.com>
2020-01-04 10:58:56 +0900

    countBiggerThanTen 함수 만들기2

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 function countBiggerThanTen(numbers) {
-
-    let count = 0;
-    numbers.forEach(n => {
-       if (n > 10) {
-           count++;
-       }
-    });
-
-    return count;
+    return numbers.filter(n => n > 10).length;
 }
 
 const count = countBiggerThanTen([1, 2, 3, 5, 10, 20, 30, 40, 50, 60]);
