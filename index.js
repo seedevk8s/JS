9646b0f9093c0894061133a81b2a7d357f65cf57
chjin <seedevk8s@gmail.com>
2020-01-04 19:06:57 +0900

    단축 평가 논리 계산법 적용예시4 -- 논리합의 경우 사용예시2

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const namelessDog = {
-    name: '',
-};
+console.log(false || 'hello');
+console.log('' || '이름없다.');
+console.log(null || '널이다');
+console.log(undefined || 'defined 되지 않았다!');
+console.log(0 || '제로다');
 
-function getName(animal) {
-    const name = animal && animal.name;
-    return name || '이름이 없는 동물입니다.';
-}
-
-const name = getName(namelessDog);
-console.log(name);
+console.log(1 || '음?');
+console.log(true || '여기 안본다.');
+console.log('와아' || '여기 안본다');
+console.log([] || '노노');
 
 
 
