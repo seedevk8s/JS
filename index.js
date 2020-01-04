1dbd8b601e8ab26cb58872a7d581f8950c199784
chjin <seedevk8s@gmail.com>
2020-01-04 10:57:15 +0900

    countBiggerThanTen 함수 만들기

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-const alphabets = ['a', 'a', 'a', 'b', 'c', 'c','d','e'];
-const counts = alphabets.reduce((acc, current) => {
-    if (acc[current]) {
-        acc[current] += 1;
-    } else {
-        acc[current] = 1;
-    }
-    return acc;
-}, {});
+function countBiggerThanTen(numbers) {
 
-console.log(counts);
+    let count = 0;
+    numbers.forEach(n => {
+       if (n > 10) {
+           count++;
+       }
+    });
+
+    return count;
+}
+
+const count = countBiggerThanTen([1, 2, 3, 5, 10, 20, 30, 40, 50, 60]);
+console.log(count);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
