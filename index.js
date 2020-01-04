ba344882684032ee3792d8369fd67789817fe55f
chjin <seedevk8s@gmail.com>
2020-01-04 19:08:30 +0900

    함수 파라미터 연습

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-console.log(false || 'hello');
-console.log('' || '이름없다.');
-console.log(null || '널이다');
-console.log(undefined || 'defined 되지 않았다!');
-console.log(0 || '제로다');
+function calculateCircleArea(r) {
+    return Math.PI * r * r;
+}
 
-console.log(1 || '음?');
-console.log(true || '여기 안본다.');
-console.log('와아' || '여기 안본다');
-console.log([] || '노노');
+const  area = calculateCircleArea(4);
+console.log(area);
+
 
 
 
