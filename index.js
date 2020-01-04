33b269abe7fc41451aac431704ea2f76ce74c351
chjin <seedevk8s@gmail.com>
2020-01-04 22:42:22 +0900

    함수 파라미터 기본값 정의함4 - 화살표 함수로 만듦

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const calculateCircleArea = (r = 1) => {
-    return Math.PI * r * r;
-}
+const calculateCircleArea = (r = 1) => Math.PI * r * r;
 
 const  area = calculateCircleArea();
 console.log(area);
