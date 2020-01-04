b1aa52a51e8dcce73ed44856bad0fba873b782a8
chjin <seedevk8s@gmail.com>
2020-01-04 22:50:41 +0900

    해당 파라미터 값이 있을때 true, 없을 때 false 출력 3

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-function isAnimal(text) {
-    const animals = ['고양이', '개', '거북이', '너구리'];
-    return animals.includes(text);
-}
+const isAnimal = text => ['고양이', '개', '거북이', '너구리'].includes(text);
 
 console.log(isAnimal('개'));
 console.log(isAnimal('노트북'));
