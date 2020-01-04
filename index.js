bb71393acb908d6df211eaa7b12d9f4522a14618
chjin <seedevk8s@gmail.com>
2020-01-04 22:54:35 +0900

    조건문 생성

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const isAnimal = text => ['고양이', '개', '거북이', '너구리'].includes(text);
+function getSound(animal) {
+    if (animal === '개') return '멍멍!';
+    if (animal === '고양이') return '야옹!';
+    if (animal === '참새') return '짹짹!';
+    if (animal === '비둘기') return '구구구구!';
+    return '...?';
+}
 
-console.log(isAnimal('개'));
-console.log(isAnimal('노트북'));
-
-
-
-
+console.log(getSound('개'));
+console.log(getSound('비둘기'));
+console.log(getSound('인간'));
