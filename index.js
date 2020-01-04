2c07e60d05b3090768ccf39883365809bbabe0c0
chjin <seedevk8s@gmail.com>
2020-01-04 12:02:12 +0900

    삼항연산자를 사용

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-const array = [1,2];
-let text = '';
-
-if (array.length === 0) {
-    text = '배열이 비어있습니다.';
-} else {
-    text = '베열이 비어있지 않습니다.';
-}
+const array = [];
+let text = array.length === 0 ? '배열이 비어있습니다.' : '배열이 비어있지 않습니다.';
 
 console.log(text);
