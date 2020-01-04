b421cd40175e3746da6f973f89a2b43b8fe130f2
chjin <seedevk8s@gmail.com>
2020-01-04 18:58:37 +0900

    단축 평가 논리 계산법 적용예시3

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-const object = { name: 'chu'};
+const namelessDog = {
+    name: '',
+};
 
-const name = object && object.name;
+function getName(animal) {
+    const name = animal && animal.name;
+    if (!name) {
+        return '이름이 없는 동물입니다.';
+    }
+    return name;
+}
+
+const name = getName(namelessDog);
 console.log(name);
-
 
 
 
