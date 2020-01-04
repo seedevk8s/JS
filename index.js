1bd9e3999b06e745190d3fd898d4871665cd7ecd
chjin <seedevk8s@gmail.com>
2020-01-04 18:52:12 +0900

    단축 평가 논리 계산법 적용예시

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-console.log(true && 'hello');
-console.log(false && 'hello');
-console.log('hello' && 'bye');
-console.log(null && 'hello');
-console.log(undefined && 'hello');
-console.log('' && 'hello');
-console.log(0 && 'hello');
-console.log(1 && 'hello');
-console.log(1 && 1);
+const object = null;
 
+const name = object && object.name;
+console.log(name);
 
 
 
