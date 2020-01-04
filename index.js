78f5a3d3e7130b42bdd0f77b7d77113751dfbf52
chjin <seedevk8s@gmail.com>
2020-01-04 11:31:43 +0900

    클래스로 Animal 생성하기

--- index.js
+++ index.js
@@ -1,45 +1,19 @@
-function Animal(type, name, sound) {
-    this.type = type;
-    this.name = name;
-    this.sound = sound;
+class Animal {
+    constructor(type, name, sound) {
+        this.type = type;
+        this.name = name;
+        this.sound =sound;
+    }
+    say() {
+        console.log(this.sound);
+    }
 }
-
-
-Animal.prototype.say = function () {
-    console.log(this.sound)
-}
-
-function Dog(name, sound) {
-    Animal.call(this, '개', name, sound);
-}
-
-function Cat(name, sound) {
-    Animal.call(this, '고양이', name, sound);
-}
-
-Dog.prototype = Animal.prototype;
-Cat.prototype = Animal.prototype;
 
 const dog = new Animal('개', '멍멍이', '멍멍');
 const cat = new Animal('고양이', '야용이', '야용');
 
 dog.say();
 cat.say();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
