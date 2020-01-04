1f34f974109039dca47f6d169601d78e4af90a45
chjin <seedevk8s@gmail.com>
2020-01-04 22:38:32 +0900

    함수 파라미터 기본값 정의함- 단축 평가 논리합 계산법 적용함.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function calculateCircleArea(r) {
-    return Math.PI * r * r;
+    const radius = r || 1;
+    return Math.PI * radius * radius;
 }
 
-const  area = calculateCircleArea(4);
+const  area = calculateCircleArea();
 console.log(area);
 
 
