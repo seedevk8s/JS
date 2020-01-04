c0cebee1782cc42d9d3b4e7c2f9472ef61c4fb7a
chjin <seedevk8s@gmail.com>
2020-01-04 18:48:42 +0900

    논리곱의 경우 앞이 true인경우 뒤의 값에 따라 결정, 앞이 false인경우는 뒤의 경우는 따져보지도 않음.3

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 console.log(true && 'hello');
 console.log(false && 'hello');
 console.log('hello' && 'bye');
+console.log(null && 'hello');
+console.log(undefined && 'hello');
+console.log('' && 'hello');
+console.log(0 && 'hello');
+console.log(1 && 'hello');
+console.log(1 && 1);
 
 
 
