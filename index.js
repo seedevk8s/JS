0f2a8bc80d27e5f3b993fbec4920b92017b58acd
chjin <seedevk8s@gmail.com>
2020-01-04 12:05:51 +0900

    삼항연산자를 사용- 중첩해서 사용가능함

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const array = [];
-let text = array.length === 0
-    ? '배열이 비어있습니다.'
-    : '배열이 비어있지 않습니다.';
+const condition1 = false;
+const condition2 = false;
 
-console.log(text);
+const value = condition1
+    ? '와우'
+    : condition2
+        ? 'blabla'
+        : 'foo';
+
+console.log(value);
