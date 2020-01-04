bf9a2045bd365e194b7486007afdc8032ed1adc7
chjin <seedevk8s@gmail.com>
2020-01-04 11:10:54 +0900

    객체 생성자 만들기2

--- index.js
+++ index.js
@@ -2,13 +2,23 @@ function Animal(type, name, sound) {
     this.type = type;
     this.name = name;
     this.sound = sound;
-    this.say = function () {
-        console.log(this.sound);
-    }
 }
+
+/*
+Animal.prototype.say = function () {
+    console.log(this.sound)
+}
+*/
 
 const dog = new Animal('개', '멍멍이', '멍멍');
 const cat = new Animal('고양이', '야용이', '야용');
+
+function say() {
+    console.log(this.sound);
+}
+
+dog.say = say;
+cat.say = say;
 
 dog.say();
 cat.say();
