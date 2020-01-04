4141284584d233e1dca3ef8491a51288c683a228
chjin <seedevk8s@gmail.com>
2020-01-04 23:08:58 +0900

    조건문 생성3

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
 function getSound(animal) {
-    switch (animal) {
-        case '개':
-            return '멍멍';
-        case '고양이':
-            return '야옹';
-        case '참새':
-            return '짹짹';
-        case '비둘기':
-            return '구구구구';
-        default:
-            return '...?'
-    }
+    const sounds = {
+        개: '멍멍!',
+        고양이: '야옹',
+        참색: '짹짹',
+        비둘기: '구구구구'
+    };
+    return sounds[animal] || '...!';
 
-
-/*    if (animal === '개') return '멍멍!';
-    if (animal === '고양이') return '야옹!';
-    if (animal === '참새') return '짹짹!';
-    if (animal === '비둘기') return '구구구구!';
-    return '...?';*/
 }
 
 console.log(getSound('개'));
