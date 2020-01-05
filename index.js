b4e0b84e75a3a5f95d4ba754933d70039f545524
chjin <seedevk8s@gmail.com>
2020-01-05 13:48:06 +0900

    setTimeout()으로 인해 4초 후에 work()함수가 실행됨.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 function work() {
-    const start = Date.now();
-    for(let i = 0; i< 1000000000; i++) {
+    setTimeout(() => {              // 백그란운드에서 실행됨=>기존흐름 막지 않음
+        const start = Date.now();
+        for(let i = 0; i< 1000000000; i++) {
 
-    }
-    const end = Date.now();
-    console.log(end - start + 'ms');
+        }
+        const end = Date.now();
+        console.log(end - start + 'ms');
+    }, 0);                          // 0 ms 후 호출해 주겠다.
+
 }
 
 work();
