4950cc2c10b2dcbfc1707864097df19babf96606
chjin <seedevk8s@gmail.com>
2020-01-05 11:47:46 +0900

    변경한 의도와 다른 결과가 나옴

--- index.js
+++ index.js
@@ -2,15 +2,11 @@ const slime = {
     name: '슬라임'
 };
 
-const cuteSlime = {
-    ...slime,
-    attribute: 'cute'
-};
+const cuteSlime = slime;
+cuteSlime.attribut = 'cute';
 
-const purpleCuteSlime = {
-    ...cuteSlime,
-    color: 'purple'
-};
+const purpleCuteSlime =cuteSlime;
+purpleCuteSlime.color = 'purple';
 
 console.log(slime);
 console.log(cuteSlime);
