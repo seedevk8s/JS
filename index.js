a521447a6cc90468c85681e115ddc6bbaf1c59af
chjin <seedevk8s@gmail.com>
2020-01-05 14:35:54 +0900

    setTimeout()으로 인해 4초 후에 work()함수가 실행됨--callback추가

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function work() {
+function work(callback) {
     setTimeout(() => {              // 백그란운드에서 실행됨=>기존흐름 막지 않음
         const start = Date.now();
         for(let i = 0; i< 1000000000; i++) {
@@ -6,12 +6,16 @@ function work() {
         }
         const end = Date.now();
         console.log(end - start + 'ms');
+        callback(end - start);
     }, 0);                          // 0 ms 후 호출해 주겠다.
 
 }
 
 console.log('작업 시작!')
-work();
+work((ms) => {
+    console.log('작업이 끝났어요!');
+    console.log(ms + 'ms 걸렸다고 해요.');
+});
 console.log('다음 작업');
 
 
