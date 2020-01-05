d55a39b5b2a5f40c4a389c8ffb438d65c4cb6d70
chjin <seedevk8s@gmail.com>
2020-01-05 12:34:42 +0900

    함수 인자에서의 spread와 rest 연산자 사용 <== spread 연산자 사용2

--- index.js
+++ index.js
@@ -2,19 +2,14 @@ function sum(...rest) {
     return rest.reduce((acc, current) => acc + current, 0);
 }
 
-console.log(sum(1,2,3,4,5,6,7,8));
+const numbers = [1,2,3,4,5,6,7,8];
+console.log(sum(...numbers));
 
 
 
 
 
-function subtract(x, y) {
-    return x - y;
-}
 
-const numbers = [1, 2];
-const result = subtract(...numbers);
-console.log(result);
 
 
 
