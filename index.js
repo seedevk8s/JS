4e9e088d8a403ed5ab360e748961393bfcd9bcda
chjin <seedevk8s@gmail.com>
2020-01-05 12:21:08 +0900

    함수파라미터에서의 spread와 rest 연산자 사용--파라미터 갯수가 불일치시 제대로 작동안함

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 function sum(a, b, c, d, e, f, g) {
-    return a + b + c + d + e + f + g;
+    let result = 0;
+    if (a) result += a;
+    if (b) result += b;
+    if (c) result += c;
+    if (d) result += d;
+    if (e) result += e;
+    if (f) result += f;
+    if (g) result += g;
+    return result;
 }
 
-console.log(sum(1,2,3,4,5,6,7));
+console.log(sum(1,2,3,4,5,6,7,8));
 
 
 
