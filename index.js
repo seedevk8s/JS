cafff029a288922234b374b32c741a75a2cd6a85
chjin <seedevk8s@gmail.com>
2020-01-05 10:30:28 +0900

    비구조화 할당

--- index.js
+++ index.js
@@ -1,28 +1,8 @@
-function makeSound(animal) {
-    const tasks = {
-        개: () => {
-            console.log('멍멍');
-        },
-        고양이() {
-            console.log('야옹');
-        },
-        비둘기() {
-            console.log('구구구구');
-        }
-    }
+const object = { a: 1, b: 2};
+const { a, b } = object;
+console.log(a);
+console.log(b);
 
-    const task = tasks[animal];
-
-    if (!task) {
-        console.log('...!');
-        return;
-    }
-    task();
-}
-
-makeSound('개');
-makeSound('비둘기');
-makeSound('인간');
 
 
 
