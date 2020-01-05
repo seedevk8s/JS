953ecfeb7a267a5546c21abf789ff5b0e955f162
chjin <seedevk8s@gmail.com>
2020-01-05 12:01:11 +0900

    ... spread 연산자를 사용 <== 배열에도 사용가능함2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const animals = ['개', '고양이', '참새'];
-const anotherAnimals = [...animals, '비둘기'];
+const anotherAnimals = animals.concat('비둘기');
 
 console.log(animals);
 console.log(anotherAnimals);
