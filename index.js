cbd25624e1cbecc730ebd3335adc5da66d3c580c
chjin <seedevk8s@gmail.com>
2020-01-05 12:29:02 +0900

    함수 인자에서의 spread와 rest 연산자 사용 -- 원소스

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ console.log(sum(1,2,3,4,5,6,7,8));
 
 
 
+function subtract(x, y) {
+    return x - y;
+}
+
+const numbers = [1, 2];
+const result = subtract(1, 2);
+console.log(result);
+
+
+
+
 
 
 
