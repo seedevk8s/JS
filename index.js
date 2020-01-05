a94cf5cb5e343bc6d7f9d31dc64dc8d195b827d6
chjin <seedevk8s@gmail.com>
2020-01-05 13:01:59 +0900

    scope의 이해2

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 const value = 'hello!';
 
 function myFunction() {
-    console.log('myFunction: ');
-    console.log(value);
-}
-
-function otherFunction() {
-    console.log('otherFunction: ');
     const value = 'bye';
-    console.log(value);
+    const anotherValue = 'world';
+    function functionInside() {
+        console.log('functionInside :');
+        console.log(value);
+        console.log(anotherValue);
+    }
+    functionInside();
 }
 
 myFunction();
-otherFunction();
-
 console.log('global scope: ');
 console.log(value);
-
-
+console.log(anotherValue);
 
 
 
